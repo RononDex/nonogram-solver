ae89839e70b778d9b26b388c80cac4b871cb1edd
Tino Heuberger <tino.heuberger@fhnw.ch>
2023-09-29 16:21:08 +0200

    Add benchamrking project (not workign yet)

---
+++ nonogram-solver-benchmarking/Benchmarks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace nonogram_solver_benchmarking
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            NonoGramSolver.Main();
+        }
+    }
+}
