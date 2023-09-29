2994194d595e00e232948164cac480b920deed38
Tino Heuberger <tino.heuberger@fhnw.ch>
2023-09-29 09:56:04 +0200

    Add test project

---
+++ nonogram-solver-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace nonogram_solver_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
